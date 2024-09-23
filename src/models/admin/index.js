ba996b8fd67822c60e00dfb18787be813545c306
Abhay Chauhan <abhaychauhan@trackier.com>
2024-09-23 16:37:23 +0530

    feat: db models and utils classes

---
+++ src/models/admin/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    constants: require("./constants"),
+    schema: require("./schema")
+}
---
+++ src/utilities/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    DBClient: require("./client"),
+    Registry: require("./registry")
+}
